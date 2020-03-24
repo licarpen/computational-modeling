ad7bed088e354cc02046094ce8760e47567c117b
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-03-24 16:29:39 -0700

    add knuckleball model and clean up code

--- src/data/models.js
+++ src/data/models.js
@@ -4,19 +4,38 @@ import radioactive_euler from '../assets/radioactive_euler.png';
 import radioactive_rk from '../assets/radioactive_rk.png';
 import prism from '../assets/prism.png';
 import chaos_pendula from '../assets/chaos_pendula';
+import knuckleball from '../assets/knuckleball';
 
 export const models = [
   {
-    title: 'Archimedes\' Pi Approximation',
-    image: pi,
-    description: 'This script approximates the value of pi to 3 decimals using Archimedes\' method. The perimeters of two polygons are calculated, with one polygon circumscribed about a circle and the other inscribed in the same circle.  The outer perimeter is n * tan(theta/2.0), where theta is the inscribed angle in radians. The inner perimeter is defined as n * sin(theta/2.0).  As n increases, the difference between the two values will become less than 1.0E-4, resulting in pi to a precision of 3 decimals. The script prints a list of values using modulo to ensure consistency and equal column widths.',
-    script: 'https://github.com/licarpen/computational-modeling/blob/master/python/pi.py'
+    title: 'Potential Surrounding Hollow Prism',
+    image: prism,
+    description: 'This script models the potential surrounding a hollow, square box of potential V = 1, with the potential set to 0 at distances far from the center of the box.  The model makes use of the Jacobi method, which is a relaxation method for solving the 2nd order differential equation which describes the situation outlined above.  Laplace\'s equation allows us to relate the potential at a specific location to the potential of areas nearby. By using a matrix to represent the potential at locations in the xy plane, the Jacobi method is used to iterate the potential at every space until the solution ceases to change. A small dv is used and the dv for each location is checked against its neighbors.  When dv has dropped below a chosen epsilon value, the process is stopped and the potential is said to be relaxed. The potential is then displayed using a spectral map.',
+    script: 'https://github.com/licarpen/computational-modeling/blob/master/python/prism.py'
+  },
+  {
+    title: 'Density of Knuckleball Placement',
+    image: knuckleball,
+    description: 'A knuckleball is a pitch thrown in baseball that minimizes spin on the ball, resulting in an erratic trajectory.  This script models a knuckleball with a spectral map showing probabilistic spatial coordinates when the ball reaches the batter, i.e. the strike zone.  This is accomplished by using Newtonian Mechanics to describe the trajectory and using the Euler method to iterate through time steps to determine the placement in the z and y directions. A histogram (not shown) displays the probability that a knuckleball will strike a particular box the size of a baseball.',
+    script: 'https://github.com/licarpen/computational-modeling/blob/master/python/knuckleball.py'
+  },
+  {
+    title: 'Chaotic Behavior of Pendula',
+    image: chaos_pendula,
+    description: 'A driven and damped pendulum will exhibit chaotic behavior if initial conditions are set appropriately. The chaotic behavior is investigated by modeling the behavior of two pendula with slightly different initial conditions. The value for theta as a function of time for a driven/damped pendulum is determined by using the Euler-Cromer method.  When log(delta_theta) is plotted against t, chaotic behavior will be evident for small time scales, and delta_theta increases linearly with time.  This is indicative of a chaotic system. Because the semi-log y plot shows a linear trend, delta_theta = exp[lambda*t), where lambda is the slope of the semi-log graph, and lambda is a parameter called the Lyapunov Exponent. This parameter is characteristic of chaotic behavior.  The Lyapunov Exponent for this behavior is determined visually to be around 0.0039, with a y-intercept of 1E-05. When the Lyapunov Exponent changes sign, the chaotic regime has been met, which is confirmed by this model.',
+    script: 'https://github.com/licarpen/computational-modeling/blob/master/python/prism.py'
   },
   {
     title: 'Mandelbrot Set',
     image: mandelbrot,
     description: 'This script generates the fractal pattern known as the Mandelbrot set.  The set displays characteristics of the set of complex, quadratic polynomials of the form f(z) = z^2 + c.  For each value of c, the sequence [0, f(0), f(f(0)), f(f(f(0)))...] is generated.  If this sequence does not diverge, the value of c is plotted.',
     script: 'https://github.com/licarpen/computational-modeling/blob/master/python/mandelbrot.py'
+  },
+  {
+    title: 'Archimedes\' Pi Approximation',
+    image: pi,
+    description: 'This script approximates the value of pi to 3 decimals using Archimedes\' method. The perimeters of two polygons are calculated, with one polygon circumscribed about a circle and the other inscribed in the same circle.  The outer perimeter is n * tan(theta/2.0), where theta is the inscribed angle in radians. The inner perimeter is defined as n * sin(theta/2.0).  As n increases, the difference between the two values will become less than 1.0E-4, resulting in pi to a precision of 3 decimals. The script prints a list of values using modulo to ensure consistency and equal column widths.',
+    script: 'https://github.com/licarpen/computational-modeling/blob/master/python/pi.py'
   },
   {
     title: 'Radioactive Decay Euler Method Error',
@@ -29,17 +48,5 @@ export const models = [
     image: radioactive_rk,
     description: 'An analytical solution for radioactive decay is well known and can be used to determine the number of atoms left in a radioactive mass after a period of time. This script uses the Euler-Cromer method for numerically calculating the error in radioactive decay number of atoms versus the time step.  The Euler-Cromer method differs from the well-known Euler method in that it uses the computed values of omega to compute the next iteration and uses a half-time step for more accuracy.  As can be seen, error increases quadratically as dt increases, but the accuracy of the model is much higher for low values of t.  Error is proportional to the order of the time step multiplied by t/dt.  Since we are keeping higher order terms, the error will be of order (dt)^3*t/dt =~ (dt)^2, which is why a quadratic correlation between error and time step is observed.',
     script: 'https://github.com/licarpen/computational-modeling/blob/master/python/radioactive_rk.py'
-  },
-  {
-    title: 'Potential Surrounding Hollow Prism',
-    image: prism,
-    description: 'This script models the potential surrounding a hollow, square box of potential V = 1, with the potential set to 0 at distances far from the center of the box.  The model makes use of the Jacobi method, which is a relaxation method for solving the 2nd order differential equation which describes the situation outlined above.  Laplace\'s equation allows us to relate the potential at a specific location to the potential of areas nearby. By using a matrix to represent the potential at locations in the xy plane, the Jacobi method is used to iterate the potential at every space until the solution ceases to change. A small dv is used and the dv for each location is checked against its neighbors.  When dv has dropped below a chosen epsilon value, the process is stopped and the potential is said to be relaxed. The potential is then displayed using a spectral map.',
-    script: 'https://github.com/licarpen/computational-modeling/blob/master/python/prism.py'
-  },
-  {
-    title: 'Chaotic Behavior of Pendula',
-    image: chaos_pendula,
-    description: 'A driven and damped pendulum will exhibit chaotic behavior if initial conditions are set appropriately. The chaotic behavior is investigated by modeling the behavior of two pendula with slightly different initial conditions. The value for theta as a function of time for a driven/damped pendulum is determined by using the Euler-Cromer method.  When log(delta_theta) is plotted against t, chaotic behavior will be evident for small time scales, and delta_theta increases linearly with time.  This is indicative of a chaotic system. Because the semi-log y plot shows a linear trend, delta_theta = exp[lambda*t), where lambda is the slope of the semi-log graph, and lambda is a parameter called the Lyapunov Exponent. This parameter is characteristic of chaotic behavior.  The Lyapunov Exponent for this behavior is determined visually to be around 0.0039, with a y-intercept of 1E-05. When the Lyapunov Exponent changes sign, the chaotic regime has been met, which is confirmed by this model.',
-    script: 'https://github.com/licarpen/computational-modeling/blob/master/python/prism.py'
   }
 ];
