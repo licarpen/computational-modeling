1636ed3b24d0e8d13787c9d7c0f52957966540c1
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-03-24 14:34:45 -0700

    refine mandelbrot script and add to models

--- python/mandelbrot.py
+++ python/mandelbrot.py
@@ -2,32 +2,25 @@ from pylab import *
 
 N = 200
 M = 50
-#define numbers for range
-
-data = zeros([N,N])
-#create matrix/array for data
+data = zeros([N, N])
 
 for i in range(N):
-	x = -2.0 + 3.0*i/(N-1.0);
-#defining the real part of the function
+	# define real part of the function
+	x = -2.0 + 3.0 * i / (N - 1.0)
 	for j in range(N):
-		y = -1.0 + 2.0*j/(N-1.0);
-#defining the imaginary part of the function
-		z0 = complex(x,y)
-#making z_0 a complex number defined by the values of x and y
+		# define imaginary part of the function
+		y = -1.0 + 2.0 * j / (N - 1.0)
+		# z_0 is a complex number defined by the values of x and y
+		z_0 = complex(x, y)
 		z = 0
-#starting at z = 0
 		for m in range(M):
+			# exclude values that diverge
 			if (abs(z)>2):
 				break
-#excluding values that sum to infinity
-			z = z*z + z0
-		data[j,i] = 1.0/m
-#including values that fall within the range desired
+			z = z * z + z_0
+		# include values that fall within desired range
+		data[j, i] = 1.0 / m
 
-imshow(data,cmap=cm.spectral,extent=[-2,1,-1,1])
-#showing data in spectral color scheme and setting coordinate bounds
+imshow(data, cmap=cm.spectral, extent=[-2, 1, -1, 1])
 colorbar()
-#including a color bar
 show()
-#show us the image!
