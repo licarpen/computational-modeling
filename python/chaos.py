a33144f131d61a7ecbcbc3eda964561c331c848e
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-03-24 16:06:12 -0700

    add chaos for pendula to models and cleaned up code

--- python/chaos.py
+++ python/chaos.py
@@ -1,70 +1,51 @@
-#Assignment Number: 03
-#Question Number: 04: Chaos and the Lyapunov Exponent
+# Chaotic Behavior of Pendula
 #Author: Lisa Carpenter
-#Filename: chaos.py
-#Date: 09.27.2011
 
-'''Abstract:A driven and damped pendulum will exhibit chaotic behavior if
-initial conditions are set appropriately. The chaotic behavior will be investigated
-by looking at the behavior of two pendulums with slightly different initial
-conditions.  The value for theta as a function of time for a driven/damped pendulum
-will be determined by using the Euler-Cromer method.  When log of the difference in
-thetas is plotted against t, chaotic behavior will be evident for small time scales,
-and the difference in thetas will increase linearly as time goes on.  This is
-indicative of a chaotic system. Because a semi-log y plot show a linear trend,
-delta theta = exp[lambda*t), where lambda is the slope of the semi log graph,
-and lambda is a parameter called the Lyapunov Exponent.  This parameter is
-characteristic of chaotic behavior.  The Lyapunov Exponent for this behavior is
-determined visually to be around 0.0039, with a y-intercept of 1E-05.  When
-the Lyapunov Exponent transitions between negative and positive values,
-the chaotic regime has been met, which is what we see in our case.  
-
-'''
+'''Abstract: A driven and damped pendulum will exhibit chaotic behavior if
+initial conditions are set appropriately. The chaotic behavior is investigated
+by modeling the behavior of two pendula with slightly different initial
+conditions. The value for theta as a function of time for a driven/damped pendulum
+is determined by using the Euler-Cromer method.  When log(delta_theta) is plotted 
+against t, chaotic behavior will be evident for small time scales, and delta_theta 
+increases linearly with time.  This is indicative of a chaotic system. Because the 
+semi-log y plot shows a linear trend, delta_theta = exp[lambda*t), where lambda is 
+the slope of the semi-log graph, and lambda is a parameter called the Lyapunov Exponent.  
+This parameter is characteristic of chaotic behavior.  The Lyapunov Exponent for this
+behavior is determined visually to be around 0.0039, with a y-intercept of 1E-05.  
+When the Lyapunov Exponent changes sign, the chaotic regime has been met, which is 
+confirmed by this model.'''
 
 from pylab import *
 
-#Set initial variables: gravity, length of pendulum, damping q, end time,
-#dt, and driving force Fd
+# initialize gravity, pendulum length, damping q, end time, dt, and driving force Fd
 g = 9.81
 l = 9.81
 q = 0.5
 t_end = 120.0
 dt = 0.01
 Fd = 1.2
-#Determine the number of steps to iterate through
-numSteps = int(t_end/dt)
-#Set up an array to pull each time step from, beginning at 0, ending at
-#t_end, with increment dt
-t = arange(0.0,t_end,dt)
-#Create arrays for two different values of theta as a function of time
+numSteps = int(t_end / dt)
+t = arange(0.0, t_end, dt)
 theta1 = zeros([numSteps])
 theta2 = zeros([numSteps])
-#Initialize theta for both arrays, differing by 0.001 in order to
-#investigate chaotic behavior for small change in initial conditions
+# initialize thetas for pendula to differ by 0.001
 theta1[0] = 0.2
 theta2[0] = 0.201
-#Create arrays for the angular frequency, omega, which will be used to
-#calculate the subsequent values of theta1 and theta2.
+# angular frequency, omega, is used to calculate subsequent values of theta1 and theta2
 omega1 = zeros([numSteps])
 omega2 = zeros([numSteps])
-#Initialize angular frequency at 0.
 omega1[0] = 0
 omega2[0] = 0
 
-#Iterate through the time steps, using the Euler-Cromer method to
-#calculate and store the angular frequencies and thetas for each t+dt.
-for n in range(1,numSteps):
-    omega1[n] = omega1[n-1]-((g/l*sin(theta1[n-1])+q*omega1[n-1]+Fd*sin((2.0/3.0)*t[n-1])))*dt
-    theta1[n] = theta1[n-1]+omega1[n]*dt
+# apply Euler-Cromer method to model omegas and thetas
+for n in range(1, numSteps):
+    omega1[n] = omega1[n - 1]-((g / l * sin(theta1[n - 1]) + q * omega1[n - 1] + Fd * sin((2.0 / 3.0) * t[n - 1]))) * dt
+    theta1[n] = theta1[n - 1] + omega1[n] * dt
 
-#Perform the same operation for theta2.
-for m in range(1,numSteps):
-    omega2[m] = omega2[m-1]-((g/l*sin(theta2[m-1])+q*omega2[m-1]+Fd*sin((2.0/3.0)*t[m-1])))*dt
-    theta2[m] = theta2[m-1]+omega2[m]*dt
-
-
-#Plot the difference between angles against time, taking the log of the
-#difference
+for m in range(1, numSteps):
+    omega2[m] = omega2[m - 1] - ((g / l * sin(theta2[m - 1])+q * omega2[m - 1] + Fd * sin((2.0 / 3.0) * t[m - 1]))) * dt
+    theta2[m] = theta2[m - 1] + omega2[m] * dt
+    
 y = exp(0.049*t)*exp(-10**-5)
 semilogy(t,(theta1 - theta2))
 plot(t,y)
--- src/data/models.js
+++ src/data/models.js
@@ -2,6 +2,8 @@ import pi from '../assets/pi.png';
 import mandelbrot from '../assets/mandelbrot.png';
 import radioactive_euler from '../assets/radioactive_euler.png';
 import radioactive_rk from '../assets/radioactive_rk.png';
+import prism from '../assets/prism.png';
+import chaos_pendula from '../assets/chaos_pendula';
 
 export const models = [
   {
@@ -27,5 +29,17 @@ export const models = [
     image: radioactive_rk,
     description: 'An analytical solution for radioactive decay is well known and can be used to determine the number of atoms left in a radioactive mass after a period of time. This script uses the Euler-Cromer method for numerically calculating the error in radioactive decay number of atoms versus the time step.  The Euler-Cromer method differs from the well-known Euler method in that it uses the computed values of omega to compute the next iteration and uses a half-time step for more accuracy.  As can be seen, error increases quadratically as dt increases, but the accuracy of the model is much higher for low values of t.  Error is proportional to the order of the time step multiplied by t/dt.  Since we are keeping higher order terms, the error will be of order (dt)^3*t/dt =~ (dt)^2, which is why a quadratic correlation between error and time step is observed.',
     script: 'https://github.com/licarpen/computational-modeling/blob/master/python/radioactive_rk.py'
+  },
+  {
+    title: 'Potential Surrounding Hollow Prism',
+    image: prism,
+    description: 'This script models the potential surrounding a hollow, square box of potential V = 1, with the potential set to 0 at distances far from the center of the box.  The model makes use of the Jacobi method, which is a relaxation method for solving the 2nd order differential equation which describes the situation outlined above.  Laplace\'s equation allows us to relate the potential at a specific location to the potential of areas nearby. By using a matrix to represent the potential at locations in the xy plane, the Jacobi method is used to iterate the potential at every space until the solution ceases to change. A small dv is used and the dv for each location is checked against its neighbors.  When dv has dropped below a chosen epsilon value, the process is stopped and the potential is said to be relaxed. The potential is then displayed using a spectral map.',
+    script: 'https://github.com/licarpen/computational-modeling/blob/master/python/prism.py'
+  },
+  {
+    title: 'Chaotic Behavior of Pendula',
+    image: chaos_pendula,
+    description: 'A driven and damped pendulum will exhibit chaotic behavior if initial conditions are set appropriately. The chaotic behavior is investigated by modeling the behavior of two pendula with slightly different initial conditions. The value for theta as a function of time for a driven/damped pendulum is determined by using the Euler-Cromer method.  When log(delta_theta) is plotted against t, chaotic behavior will be evident for small time scales, and delta_theta increases linearly with time.  This is indicative of a chaotic system. Because the semi-log y plot shows a linear trend, delta_theta = exp[lambda*t), where lambda is the slope of the semi-log graph, and lambda is a parameter called the Lyapunov Exponent. This parameter is characteristic of chaotic behavior.  The Lyapunov Exponent for this behavior is determined visually to be around 0.0039, with a y-intercept of 1E-05. When the Lyapunov Exponent changes sign, the chaotic regime has been met, which is confirmed by this model.',
+    script: 'https://github.com/licarpen/computational-modeling/blob/master/python/prism.py'
   }
 ];
