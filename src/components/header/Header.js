d22d5014163d0e64471de7073b7d246c3c9c9027
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-03-25 22:01:27 -0700

    fix capitalization error in Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <div className={styles.Header}>
       <h1 className={styles.title}>Computational Modeling</h1>
       <h2 className={styles.author}>Code by Lisa Carpenter</h2>
-      <p className={styles.description}>Simulations of Physical Systems from classical mechanics, electricity and magnetism, chaos, and statistical mechanics</p>
+      <p className={styles.description}>Simulations of physical systems from classical mechanics, electricity and magnetism, chaos, and statistical mechanics</p>
     </div>
   );
 };
