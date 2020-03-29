8d48661293bc5b0e36885ec160e8a70d63e3af93
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-03-29 14:33:47 -0700

    enhance responsiveness of model items

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className={styles.Header}>
-      <h1 className={styles.title}>Computational Modeling</h1>
-      <h2 className={styles.author}>Code by Lisa Carpenter</h2>
-      <p className={styles.description}>Simulations of physical systems from classical mechanics, electricity and magnetism, chaos, and statistical mechanics</p>
+      <Link className={styles.Link} to={'/'}>
+        <h1 className={styles.title}>Computational Modeling</h1>
+        <h2 className={styles.author}>Code by Lisa Carpenter</h2>
+        <p className={styles.description}>Simulations of physical systems from classical mechanics, electricity and magnetism, chaos, and statistical mechanics</p>
+      </Link>
     </div>
   );
 };
